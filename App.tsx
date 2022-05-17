01f3754ff2259199e7159b0b2df850cdc8f3b7df
kasutu <jeromecabugwason2@protonmail.com>
2022-05-17 19:33:42 +0800

    fixed ("react"' has no exported member 'Node'.)

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import type { Node } from 'react';
+import type { ReactNode } from 'react';
 import AddLocationMap from './src/screens/userSeller/addLocationMap.screen';
 
-export default function App(): Node {
+export default function App(): ReactNode {
   return <AddLocationMap />;
 }
