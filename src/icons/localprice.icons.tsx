38ed59459057707da6f72f7aad8dbfa00fce065b
kasutu <jeromecabugwason2@protonmail.com>
2022-05-19 14:01:57 +0800

    moved icons

--- src/icons/localprice.icons.tsx
+++ src/icons/localprice.icons.tsx
@@ -1,7 +1,9 @@
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import IonIcon from 'react-native-vector-icons/Ionicons'
 import React from 'react';
 
 const arrowBack = <Icon name="arrow-back" size={30} color="black" />;
 const addIcon = <Icon name="add" size={25} color="white" />;
+const eyeIcon = <Icon name="add" size={25} color="white" />;
 
 export { arrowBack, addIcon };
