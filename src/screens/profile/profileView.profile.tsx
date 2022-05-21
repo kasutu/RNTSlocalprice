f97750219aee4076cd796b18c69e9d4fb3d902f3
Jiv <104212456+jivayydelacruz@users.noreply.github.com>
2022-05-22 00:39:53 +0800

    Updated Profile View Screen

--- src/screens/profile/profileView.profile.tsx
+++ src/screens/profile/profileView.profile.tsx
@@ -7,8 +7,26 @@ export default function ProfileView() {
   return (
     <NativeBaseProvider>
       <Box safeArea width={'full'} height={'full'}>
-        <VStack space={2} width={'full'} height={'full'}>
+        <VStack width={'full'} height={'full'}>
           <TitleHeader title="Profile" />
+          <Center
+            alignContent={'center'}
+            textAlign={'center'}
+            marginY={'3'}
+            width={'full'}
+            height={'30px'}
+          >
+            Profile Icon
+          </Center>
+          <Center
+            alignContent={'center'}
+            textAlign={'center'}
+            width={'full'}
+            height={'50px'}
+            backgroundColor={'violet.300'}
+          >
+            Anonymous
+          </Center>
           <Center
             marginY={'3'}
             bottom={'20'}
