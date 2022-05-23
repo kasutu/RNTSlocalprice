629225c8af9e2e3cf910f48cb745be2fe2ae7495
Dave00913 <deybalivio09@gmail.com>
2022-05-23 11:10:08 +0800

    Icon update

--- src/icons/localprice.icons.tsx
+++ src/icons/localprice.icons.tsx
@@ -1,8 +1,10 @@
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign.js';
+import Icon from 'react-native-vector-icons/MaterialIcons.js';
 import React from 'react';
 
 const arrowBack = <Icon name="arrow-back" size={30} color="black" />;
 const addIcon = <Icon name="add" size={25} color="white" />;
 const eyeIcon = <Icon name="remove-red-eye" size={25} color="black" />;
+const heartIcon = <AntDesignIcon name="heart" size={25} color="#9e6dde" />;
 
-export { arrowBack, addIcon, eyeIcon };
+export { arrowBack, addIcon, eyeIcon, heartIcon};
