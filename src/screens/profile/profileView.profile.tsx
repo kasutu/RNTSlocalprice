18e2da3364a8d6175d0386599e1d6a4091996799
Jiv <104212456+jivayydelacruz@users.noreply.github.com>
2022-05-24 00:30:28 +0800

    Extracted reusable icons

--- src/screens/profile/profileView.profile.tsx
+++ src/screens/profile/profileView.profile.tsx
@@ -1,7 +1,21 @@
-import { Box, Center, NativeBaseProvider, Text, VStack } from 'native-base';
+import {
+  Box,
+  Center,
+  Icon,
+  NativeBaseProvider,
+  VStack,
+  HStack
+} from 'native-base';
 import React from 'react';
 import SolidButton from '../../components/buttons/solid.button';
 import { TitleHeader } from '../../components/header/titleAndBackButton.header';
+import {
+  hollowBagIcon,
+  hollowChatIcon,
+  hollowHomeIcon,
+  hollowNotifIcon,
+  profileIcon
+} from '../../icons/localprice.icons';
 
 export default function ProfileView() {
   return (
@@ -16,37 +30,35 @@ export default function ProfileView() {
             width={'full'}
             height={'30px'}
           >
-            Profile Icon
+            {<Icon size={'45px'} as={profileIcon} />}
           </Center>
-          <Center
-            alignContent={'center'}
-            textAlign={'center'}
-            width={'full'}
-            height={'50px'}
-            backgroundColor={'violet.300'}
-          >
-            Anonymous
+          <Center width={'full'}>
+            <Center
+              _text={{ textAlign: 'center' }}
+              alignContent={'center'}
+              textAlign={'center'}
+              width={'190px'}
+              backgroundColor={'#D5C1F1'}
+              borderRadius={'10'}
+            >
+              Anonymous
+            </Center>
           </Center>
           <Center
             marginY={'3'}
-            bottom={'40px'}
+            bottom={'3'}
             position={'absolute'}
             width={'full'}
             maxWidth={'full'}
           >
-            <SolidButton value="Log In or Register" />
-          </Center>
-          <Center
-            alignContent={'center'}
-            textAlign={'center'}
-            width={'full'}
-            height={'50px'}
-            marginY={'3'}
-            bottom={'1'}
-            position={'absolute'}
-            maxWidth={'full'}
-          >
-            Icons
+            <SolidButton value="Log In or Register" />{' '}
+            <HStack space={7} justifyContent={'center'} width={'full'}>
+              <Icon size={'35px'} as={hollowHomeIcon} />
+              <Icon size={'35px'} as={hollowChatIcon} />
+              <Icon size={'35px'} as={hollowBagIcon} />
+              <Icon size={'35px'} as={hollowNotifIcon} />
+              <Icon size={'35px'} as={profileIcon} />
+            </HStack>
           </Center>
         </VStack>
       </Box>
