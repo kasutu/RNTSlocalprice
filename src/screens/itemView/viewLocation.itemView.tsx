9ea167144bb89a33b65bd7d519a6d467d64b2569
Dave00913 <deybalivio09@gmail.com>
2022-05-25 18:34:07 +0800

    viewLoc update

--- src/screens/itemView/viewLocation.itemView.tsx
+++ src/screens/itemView/viewLocation.itemView.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { Button } from 'native-base';
 import { ButtonProps } from '../../types/buttonProps.type';
 
-<<<<<<< HEAD
 export default function ViewLocationButton({
-=======
-export default function ViewLocation({
->>>>>>> f124cf4ee2e92db1787c35fc383ef4243c81f1f4
   value,
   borderColor,
   onPressHandler
@@ -14,7 +10,6 @@ export default function ViewLocation({
   return (
     <>
       <Button
-<<<<<<< HEAD
         width={'100px'}
         height={'10'}
         variant={'outline'}
@@ -23,16 +18,6 @@ export default function ViewLocation({
         }
         _text={{ fontSize: '12', fontWeight: 'light' }}
         borderRadius={'xxl'}
-=======
-        width={'87'}
-        height={'10'}
-        variant={'outline'}
-        onPress={
-          onPressHandler ? onPressHandler : () => console.log('CANCEL pressed')
-        }
-        _text={{ fontSize: '15', fontWeight: 'medium' }}
-        borderRadius={'50'}
->>>>>>> f124cf4ee2e92db1787c35fc383ef4243c81f1f4
         borderColor={borderColor}
       >
         {value}
