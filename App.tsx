ae9bcc9a96d0a330da291416bfa850312da7f794
Jerome Cabugwason <jeromecabugwason2@protonmail.com>
2022-05-27 16:24:50 +0800

    fixed the profileview duplicate

--- App.tsx
+++ App.tsx
@@ -28,7 +28,6 @@ export default function App(): ReactNode {
       {/* <ResetPasswordView/> */}
       {/* <ProfileView/> */}
       {/* <MainHome/> */}
-      {/* <ProfileView/> */}
 
       {/* -------------- test section -------------- */}
 
