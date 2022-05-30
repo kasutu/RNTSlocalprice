473c9df7b3f410bc42bbd53ef635962fd0b757d6
avengersassemble2019 <amsobasil@gmail.com>
2022-05-30 23:44:30 +0800

    startingOfTheCode

---
+++ src/model/card.ts
@@ -0,0 +1,20 @@
+export class Cards {
+  public username: string;
+  public password: string;
+  public id: string;
+
+  constructor(username: string, password: string, id: string) {
+    this.username = username;
+    this.password = password;
+    this.id = id;
+  }
+}
+function addCards() {
+  class addCards {
+    addItem!: [];
+    constructor(addItem: []) {
+      this.addItem;
+      addItem;
+    }
+  }
+}
