544cf89e7516a17335bbf06aadfd73d3e787d9f5
kasutu <jeromecabugwason2@protonmail.com>
2022-06-01 00:46:56 +0800

    added chat details screen
    missing 2 btns will add tommorow

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
+import AppTemp from './AppTemp';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+// for building screens temporarily
+AppRegistry.registerComponent(appName, () => AppTemp);
