06cc39ed2a723f3e39becdb6e229d27f0d635aef
kasutu <jeromecabugwason2@protonmail.com>
2022-06-02 22:50:33 +0800

    added code review notes

--- src/model/order.ts
+++ src/model/order.ts
@@ -1,5 +1,8 @@
 import { Transaction } from './transaction';
 export class Order extends Transaction {
   public parcel?: [];
-  public paymentOption?: string;
+  public paymentOption?: string; 
+  // order completion property boolean
 }
+
+// make a notif store
