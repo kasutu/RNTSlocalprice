9176d7a1967cc7e29ca4938cbd3cedfe8eb39cb9
avengersassemble2019 <amsobasil@gmail.com>
2022-06-02 23:41:40 +0800

    fixed review issues

--- src/model/order.ts
+++ src/model/order.ts
@@ -1,8 +1,13 @@
 import { Transaction } from './transaction';
-export class Order extends Transaction {
-  public parcel?: [];
-  public paymentOption?: string; 
-  // order completion property boolean
+export abstract class Order extends Transaction {
+  public parcel: [];
+  public cod: string;
+  public orderCompleted: boolean;
+  
+  constructor(id: string, sellerId: string, buyerId: string ) {
+    super(id, buyerId, sellerId);
+    this.parcel = [];
+    this.cod = '90';
+    this.orderCompleted = false;
+  }
 }
-
-// make a notif store
