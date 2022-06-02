d6634bb5c431123057aabf3a83cd98f936349e83
kasutu <jeromecabugwason2@protonmail.com>
2022-06-02 22:19:42 +0800

    added code review

--- src/model/transaction.ts
+++ src/model/transaction.ts
@@ -1,13 +1,13 @@
 export class Transaction {
-  public id: string;
-  public buyerId: string;
-  public sellerId: string;
+  public id: string = '';
+  public buyerId: string = '';
+  public sellerId: string = '';
 
 
-  constructor(id: string, buyerId: string, sellerId: string) {
-    this.id = id;
-    this.buyerId = buyerId;
-    this.sellerId = sellerId;
-  }
+  // constructor(id: string, buyerId: string, sellerId: string) {
+  //   this.id = id;
+  //   this.buyerId = buyerId;
+  //   this.sellerId = sellerId;
+  // }
 }
 
