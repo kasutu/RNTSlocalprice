a02e4815ba2ed4305e20c3ef0a94971c2735e701
avengersassemble2019 <amsobasil@gmail.com>
2022-06-02 21:28:57 +0800

    some changes on cards

--- src/model/card.ts
+++ src/model/card.ts
@@ -1,20 +1,13 @@
 export class Cards {
-  public username: string;
-  public password: string;
   public id: string;
 
-  constructor(username: string, password: string, id: string) {
-    this.username = username;
-    this.password = password;
-    this.id = id;
+  constructor(id: string) {
+  this.id = id;
   }
 }
 function addCards() {
-  class addCards {
-    addItem: [];
-
-    constructor(addItem: []) {
-      this.addItem = addItem;
-    }
+  return new Cards(){
+    
+  }
   }
 }
